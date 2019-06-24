fcd179ee3192ecd4dbeeb3e3d17d719756ae826f
Viktor Plattner <plattnerviktor@gmail.com>
2019-06-24 19:58:24 +0100

    missing data points, stage tranzition plot, small changes

--- analysis.R
+++ analysis.R
@@ -25,10 +25,21 @@ TRAINING <- TRAINING %>%
       )
     )
   )) %>%
+  mutate(session = ifelse(animal_id %in% c("AA01", "AA02", "DO01", "DO02", "SC01", "SC02"), yes = "session_1",
+    ifelse(animal_id %in% c("VP01", "VP02", "AA03", "AA04", "DO03", "DO04"), yes = "session_2",
+      ifelse(animal_id %in% c("SC03", "SC04", "VP03", "VP04", "AA05", "AA06"), yes = "session_3",
+        ifelse(animal_id %in% c("DO05", "DO06", "SC05", "SC06", "VP05", "VP06"), yes = "session_4",
+          ifelse(animal_id %in% c("AA07", "AA08", "DO07", "DO08", "VP07", "VP08"), yes = "session_5", no = NA)
+        )
+      )
+    )
+  )) %>%
   gather(right_trials, left_trials, key = "choice_direction", value = "No_pokes")
-
 # ifelse(animal_id == "AA01" | "AA02" | "DO01" | "DO02" | "SC01" | "SC02", yes = 1, no = NA)
 TRAINING$session_length %>% hist(breaks = 50)
+
+
+
 
 
 
@@ -82,7 +93,7 @@ ggplot(
 ### PLOT: CP duration ----
 ggplot(
   data = TRAINING %>%
-    dplyr::filter(stage == 2, date > "2019-06-10"),
+    dplyr::filter(stage == 1),
 
   mapping = aes(
     col = animal_id,
@@ -99,8 +110,8 @@ ggplot(
   scale_x_date(
     date_breaks = "1 day",
     date_labels = "%b %d",
-    minor_breaks = "1 day",
-    limits = c(as.Date("2019-06-18"), as.Date("2019-06-21"))
+    minor_breaks = "1 day"
+    #limits = c(as.Date("2019-06-18"), as.Date("2019-06-21"))
   ) +
   theme(axis.text.x = element_text(angle = 45, vjust = 0.5)) +
   ylab("CP duration (stage 1) [s]") +
@@ -108,10 +119,12 @@ ggplot(
   geom_label_repel(
     data = TRAINING %>%
       dplyr::filter(
-        stage == 2,
-        date == "2019-06-19" | date == "2019-06-20" | date == "2019-06-21",
-        choice_direction == "right_trials",
-        total_CP > 6
+        stage == 1,
+        date == "2019-06-24",
+          #max(date),
+        #"2019-06-19" | date == "2019-06-20" | ,
+        choice_direction == "right_trials"
+        #total_CP > 6
       ), # %>%
     # arrange(total_CP) %>%
     # slice(1:6),
@@ -124,11 +137,13 @@ ggplot(
   ) +
   geom_hline(yintercept = 6)
 
-
+TRAINING %>% 
+  dplyr::filter(date == "21-06-2019") %>%
+  select(stage)
 
 ### PLOT: No. done trials vs date ----
 ggplot(
-  data = TRAINING %>% dplyr::filter(stage == 2, date > "2019-06-01"),
+  data = TRAINING %>% dplyr::filter(stage == 1),
   mapping = aes(
     x = date,
     y = done_trials / ((session_length * 60 * 24) %>% as.numeric()) # normalized to session length
@@ -167,10 +182,22 @@ ggplot(
 ### PLOT: CP duration vs No. done trials ----
 ggplot(
   data = TRAINING %>% dplyr::filter(stage == 1),
-  mapping = aes(x = done_trials, y = total_CP)
+  mapping = aes(y = done_trials, x = total_CP)
 ) +
   geom_point(mapping = aes(col = animal_id))
 geom_line(mapping = aes(col = animal_id))
+
+
+### PLOT: stage tranzition track
+ggplot(data = TRAINING,
+       mapping = aes(x = date, y = animal_id)) + 
+  geom_point(aes(col = as.character(stage)), size = 6)+
+  scale_x_date(date_breaks = "1 day", date_labels = "%b %d", minor_breaks = "1 day") +
+  theme(axis.text.x = element_text(angle = 90, vjust = -0.001))
+ 
+  geom_label_repel(aes(label = animal_id),
+                   direction = "y",
+                   hjust = -0.5)
 
 
 
@@ -180,7 +207,7 @@ recording_dates <- TRAINING %>%
   dplyr::filter(choice_direction == "left_trials") %>%
   group_by(animal_id) %>%
   mutate(trained = T) %>%
-  pad(start_val = as.Date("2019-04-25"), end_val = as.Date("2019-06-21")) %>%
+  pad(start_val = as.Date("2019-04-25"), end_val = as.Date("2019-06-24")) %>%
   mutate(day_name = weekdays(date)) %>%
   mutate(weekend = is.weekend(date)) %>%
   select(date, day_name, trained, rig) %>%
@@ -196,10 +223,19 @@ recording_dates <- TRAINING %>%
         )
       )
     )
+  )) %>%
+  mutate(session = ifelse(animal_id %in% c("AA01", "AA02", "DO01", "DO02", "SC01", "SC02"), yes = "session_1",
+    ifelse(animal_id %in% c("VP01", "VP02", "AA03", "AA04", "DO03", "DO04"), yes = "session_2",
+      ifelse(animal_id %in% c("SC03", "SC04", "VP03", "VP04", "AA05", "AA06"), yes = "session_3",
+        ifelse(animal_id %in% c("DO05", "DO06", "SC05", "SC06", "VP05", "VP06"), yes = "session_4",
+          ifelse(animal_id %in% c("AA07", "AA08", "DO07", "DO08", "VP07", "VP08"), yes = "session_5", no = NA)
+        )
+      )
+    )
   ))
 
 
-ggplot(recording_dates, aes(x=rig,y=animal_id))+geom_point()
+ggplot(recording_dates, aes(x = rig, y = animal_id)) + geom_point()
 
 
 
@@ -208,10 +244,12 @@ ggplot(recording_dates, aes(x=rig,y=animal_id))+geom_point()
 
 
 
-recording_dates$rig %>% na.locf0()
+# recording_dates$rig %>% na.locf0()
 
+
+### PLOT: missing data points track
 ggplot(
-  data = recording_dates %>% 
+  data = recording_dates %>%
     mutate(animal_id = fct_reorder(animal_id, as.numeric(rig))),
   mapping = aes(x = date, y = animal_id)
 ) +
@@ -225,9 +263,21 @@ ggplot(
     direction = "y",
     hjust = -1
   ) +
-  labs(fill = "Rig")
-  # scale_color_brewer(
-  #   palette = "Set3",
-  #   name = "Animal"
-  #   # guide = FALSE
-  # )
+  labs(fill = "Rig") 
+  geom_text_repel(
+    data = recording_dates %>% dplyr::filter(date == max(date)),
+    mapping = aes(label = session, fill = as.character(session)),
+    direction = "y",
+    hjust = -3
+    )
+
+
+
+
+
+
+# scale_color_brewer(
+#   palette = "Set3",
+#   name = "Animal"
+#   # guide = FALSE
+# )
