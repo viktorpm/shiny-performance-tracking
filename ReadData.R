82c9fbd8b41200127013b7e17fec3bfdf11c8891
Viktor Plattner <plattnerviktor@gmail.com>
2019-07-11 11:36:19 +0100

    cleaned from unused, commented lines

--- ReadData.R
+++ ReadData.R
@@ -114,49 +114,6 @@ ReadData <- function(rds_file) {
   
   TRAINING <- lapply(TRAINING, function(x) ifelse(is_empty(x), yes = "empty_field_in_mat_file", no = x)) 
 
-  
-  # if (identical(TRAINING$settings_file, character(0))){
-  #   TRAINING$settings_file <- "empty_field_in_mat_file"
-  # }
-  # 
-  # if (identical(TRAINING$file, character(0))){
-  #    TRAINING$file <- "empty_field_in_mat_file"
-  # }
-   
-  
-  
-  #browser()
 
-
-
-
-  # rat_data$saved[, , ]$SideSection TrainingStage, init CP duration, total CP duration
-  # rat_data$saved[, , ]$ProtocolSection n done trials
-
-
-
-  # # adding rows of data to tibble
-  # TRAINING <- add_row(TRAINING,
-  #   # data id
-  #   experimenter = rat_data$saved[, , ]$SavingSection.experimenter,
-  #   animal_id = rat_data$saved[, , ]$SavingSection.ratname,
-  #   date = rat_data$saved[, , ]$SavingSection.SaveTime %>%
-  #     as.character() %>%
-  #     substr(1, 11), # %>%
-  #   # strptime(format = "%d-%b-%Y") %>%
-  #   # as.POSIXct(),
-  #   time = rat_data$saved[, , ]$SavingSection.SaveTime %>%
-  #     as.character() %>%
-  #     substr(13, 20),
-  #   right_trials = rat_data$saved[, , ]$StimulusSection.nTrialsClass1 +
-  #     rat_data$saved[, , ]$StimulusSection.nTrialsClass2 +
-  #     rat_data$saved[, , ]$StimulusSection.nTrialsClass3 +
-  #     rat_data$saved[, , ]$StimulusSection.nTrialsClass4,
-  #   left_trials = rat_data$saved[, , ]$StimulusSection.nTrialsClass5 +
-  #     rat_data$saved[, , ]$StimulusSection.nTrialsClass6 +
-  #     rat_data$saved[, , ]$StimulusSection.nTrialsClass7 +
-  #     rat_data$saved[, , ]$StimulusSection.nTrialsClass8
-  # )
-  #
   return(TRAINING)
 }
