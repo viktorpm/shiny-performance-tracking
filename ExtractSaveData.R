b8c6424acf8b1fa7d43a077c1465f4fd4af2af58
Viktor Plattner <plattnerviktor@gmail.com>
2021-01-18 13:46:46 +0000

    trial by trial analysis

--- ExtractSaveData.R
+++ ExtractSaveData.R
@@ -12,7 +12,10 @@ file_list <- list.files(file.path("D:", "_Rig_data", "SoloData", "Data"),
                         pattern = "\\.mat$",
                         recursive = T) %>% 
   as.list()
-file_list <- file_list[!grepl("experimenter", file_list)]# & !grepl("emmett", file_list)] # excluding files from experimenter folder
+file_list <- file_list[!grepl("experimenter", file_list) & 
+                         !grepl("Session Settings", file_list) & 
+                         !grepl("FakeSubject", file_list)]
+# & !grepl("emmett", file_list)] # excluding files from experimenter folder
 full_path <- paste0(in_path, "/", file_list) %>% as.list()
 
 ### converting mat files to rds ----
@@ -24,4 +27,31 @@ walk(full_path, ~ ConvertToRDS(file = .x))
 ### reading rds files to a tibble and saving them to a csv file ----
 rds_list <- list.files(file.path("D:", "_R_WD", "git_projects", "r_codes_rat_wm", "data", "rds_files")) %>% as.list()
 walk(rds_list, ~ ReadData(rds_file = .x) %>% TRAININGtoCSV())
+
+
+
+################################################
+### Trial by trial data 
+
+walk(rds_list, ~ ReadData(rds_file = .x, trialData = T) %>% 
+       TRAININGtoCSV(filename = "TrialByTrial.csv"))
+
+
+# data_@AltStatDelayComp_athena_AA08_200217a.mat.rds
+# data_@AthenaDelayComp_viktor_VP12_200213a.mat.rds
+# data_@SoundCategorization_athena_AA05_200217a.mat.rds
+# data_@AltSoundCategorization_sharbat_SC04_200106a.mat.rds
+# LT01_Gap_Detection_20191011_093950.mat.rds
+# 
+# 
+# ReadData(rds_file = "LT01_Gap_Detection_20191011_093950.mat.rds",trialData = T) %>%
+#   TRAININGtoCSV(filename = "TrialByTrial.csv")
+
+
+
+
+
+
+
+
 
--- ReadData.R
+++ ReadData.R
@@ -1,67 +1,130 @@
-ReadData <- function(rds_file) {
+ReadData <- function(rds_file, trialData = F) {
   library(R.matlab)
   library(tidyverse)
   library(purrr)
 
+  if (missing(trialData)){
+    trialData = F
+  }
+  
   # browser()
-
   path <- file.path(
     "shiny_app"
   )
-
-
   data_source <- NA
+  
+  ### Default mode, no trial data ----
+  if (trialData == F) {
 
-  ### cheks if CSV file exists
-  csv_exists <- file.exists(file.path(path, "TRAINING.csv"))
+    ### cheks if CSV file exists
+    csv_exists <- file.exists(file.path(path, "TRAINING.csv"))
 
-  ### cheks if the rds_file has been processed (can be found in the CSV file)
-  if (csv_exists == T) {
-    read_TRAINING <- suppressMessages(
-      suppressWarnings(
-        read_csv(file.path(path, "TRAINING.csv"))
+    ### cheks if the rds_file has been processed (can be found in the CSV file)
+    if (csv_exists == T) {
+      read_TRAINING <- suppressMessages(
+        suppressWarnings(
+          read_csv(file.path(path, "TRAINING.csv"))
+        )
       )
-    )
-    file_processed_test <- str_detect(
-      read_TRAINING$file,
-      regex(paste0(rds_file))
-    )
-  } else {
-    file_processed_test <- F
-  }
-
-
-  if (all(file_processed_test == F)) {
-    rat_data <- readRDS(paste0(file.path("data", "rds_files"), "/", rds_file))
-
-    ### checks the source of the data
-    if (rat_data %>% names() %in% "SessionData" %>% any()) {
-      data_source <- "bpod"
-      source("ReadBpodData.R")
-      TRAINING <- ReadBpodData(
-        rds_file = rds_file,
-        data_source = data_source,
-        rat_data =rat_data
+      file_processed_test <- str_detect(
+        read_TRAINING$file,
+        regex(paste0(rds_file))
       )
-      return(TRAINING)
     } else {
-      if (rat_data %>% names() %in% "saved" %>% any()) {
-        data_source <- "bcontrol"
-        source("ReadBcontrolData.R")
-        TRAINING <- ReadBcontrolData(
+      file_processed_test <- F
+    }
+
+
+    if (all(file_processed_test == F)) {
+      rat_data <- readRDS(paste0(file.path("data", "rds_files"), "/", rds_file))
+
+      ### checks the source of the data
+      if (rat_data %>% names() %in% "SessionData" %>% any()) {
+        data_source <- "bpod"
+        source("ReadBpodData.R")
+        TRAINING <- ReadBpodData(
           rds_file = rds_file,
-          rat_data = rat_data,
-          data_source = data_source
+          data_source = data_source,
+          rat_data = rat_data
         )
         return(TRAINING)
+      } else {
+        if (rat_data %>% names() %in% "saved" %>% any()) {
+          data_source <- "bcontrol"
+          source("ReadBcontrolData.R")
+          TRAINING <- ReadBcontrolData(
+            rds_file = rds_file,
+            rat_data = rat_data,
+            data_source = data_source
+          )
+          return(TRAINING)
+        }
       }
+    } else {
+      warning(paste0(
+        rds_file,
+        # ".mat: ",
+        " file has already been processed"
+      ))
+      return(NULL)
     }
-  } else {
-    warning(paste0(
-      rds_file,
-      # ".mat: ",
-      " file has already been processed"
-    ))
-    return(NULL)
+  } 
+  
+  ### With trial data ----
+  if(trialData == T) {
+
+
+    ### cheks if CSV file exists
+    csv_exists <- file.exists(file.path(path, "TrialByTrial.csv"))
+
+    ### cheks if the rds_file has been processed (can be found in the CSV file)
+    if (csv_exists == T) {
+      read_TRIAL <- suppressMessages(
+        suppressWarnings(
+          read_csv(file.path(path, "TrialByTrial.csv"))
+        )
+      )
+      file_processed_test <- str_detect(
+        read_TRIAL$file,
+        regex(paste0(rds_file))
+      )
+    } else {
+      file_processed_test <- F
+    }
+
+
+    if (all(file_processed_test == F)) {
+      rat_data <- readRDS(paste0(file.path("data", "rds_files"), "/", rds_file))
+
+      ### checks the source of the data
+      if (rat_data %>% names() %in% "SessionData" %>% any()) {
+        # data_source <- "bpod"
+        # source("ReadBpodData.R")
+        # TRIAL <- ReadBpodData(
+        #   rds_file = rds_file,
+        #   data_source = data_source,
+        #   rat_data = rat_data
+        # )
+        return(NULL)
+      } else {
+        if (rat_data %>% names() %in% "saved" %>% any()) {
+          data_source <- "bcontrol"
+          source("ReadTrialData.R")
+          TRIAL <- ReadTrialData(
+            rds_file = rds_file,
+            rat_data = rat_data,
+            data_source = data_source
+          )
+          return(TRIAL)
+        }
+      }
+    } else {
+      warning(paste0(
+        rds_file,
+        # ".mat: ",
+        " file has already been processed"
+      ))
+      return(NULL)
+    }
   }
 }
