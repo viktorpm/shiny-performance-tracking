d9dc1fc989e104fc74a939c5cbf1190608c0c203
Viktor Plattner <plattnerviktor@gmail.com>
2021-02-25 15:36:43 +0000

    optimising file conversion, only not yet converted and processed files are sent to ConvertToRDS, ReadData and TRAININGtoCSV functions

--- ExtractSaveData.R
+++ ExtractSaveData.R
@@ -3,30 +3,73 @@
 source("ConvertToRDS.R") ### f. to convert mat files to rds
 source("ReadData.R") ### f. to read rds files to a tibble
 source("TRAININGtoCSV.R") ### f. to save tibble to csv
+# source("TRAININGtoFeather.R") ### f. to save tibble to csv
 
 
 
 ### determining the full path of each file ----
+
+### listing all the mat files in the data folders
 in_path <- file.path("D:", "_Rig_data", "SoloData", "Data")
 file_list <- list.files(file.path("D:", "_Rig_data", "SoloData", "Data"), 
                         pattern = "\\.mat$",
                         recursive = T) %>% 
   as.list()
-file_list <- file_list[!grepl("experimenter", file_list) & 
+
+### removing test data
+file_list <- file_list[!grepl("experimenter", file_list) & # excluding files from experimenter folder
                          !grepl("Session Settings", file_list) & 
                          !grepl("FakeSubject", file_list)]
-# & !grepl("emmett", file_list)] # excluding files from experimenter folder
-full_path <- paste0(in_path, "/", file_list) %>% as.list()
+# & !grepl("emmett", file_list)] 
+# full_path <- paste0(in_path, "/", file_list) %>% as.list()
 
-### converting mat files to rds ----
-walk(full_path, ~ ConvertToRDS(file = .x))
-
-
-
-### reading the converted rds filenames to a list
-### reading rds files to a tibble and saving them to a csv file ----
+### listing files already converted to rds 
 rds_list <- list.files(file.path("D:", "_R_WD", "git_projects", "r_codes_rat_wm", "data", "rds_files")) %>% as.list()
-walk(rds_list, ~ ReadData(rds_file = .x) %>% TRAININGtoCSV())
+
+### listing the not yet converted mat files (remove ".rds", remove path form character strings)
+### difference between file_list and rds_list
+not_yet_conv <- setdiff(
+  sub(pattern = ".*\\/", "", file_list),  
+  rds_list %>% unlist() %>% substr(start = 1, stop = nchar(.)-4)
+) 
+
+
+### adding path to the not yet converted mat files
+ifelse(
+  not_yet_conv %>% is_empty(),
+  to_be_conv <-  character(),
+  to_be_conv <- paste0(
+    in_path, 
+    "/",
+    file_list[str_detect(file_list, pattern = paste(not_yet_conv, collapse = "|")
+    )] %>% unlist()
+  ) 
+  )
+
+
+
+
+### converting not yet converted mat files to rds ----
+ifelse(
+  to_be_conv %>% is_empty(),
+  warning("Nothing to convert"),
+  walk(to_be_conv, ~ ConvertToRDS(file = .x))
+)
+
+
+### reading rds files to a tibble and saving them to a csv file ----
+
+### updated rds_list
+rds_list <- list.files(file.path("D:", "_R_WD", "git_projects", "r_codes_rat_wm", "data", "rds_files")) %>% as.list()
+
+to_append <- setdiff(
+  rds_list %>% unlist(),
+  read_csv(file.path("shiny_app", "TRAINING.csv")) %>% dplyr::select(file) %>% pull()
+  ) %>%
+  as.list()
+
+walk(to_append, ~ ReadData(rds_file = .x) %>% TRAININGtoCSV())
+# walk(rds_list, ~ ReadData(rds_file = .x) %>% TRAININGtoFeather())
 
 
 
