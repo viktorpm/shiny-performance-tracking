e1195e321d5bef15acef4ef86e49c00952f75fa9
Viktor Plattner <plattnerviktor@gmail.com>
2021-03-16 16:32:17 +0000

    small changes

--- training_and_watering_visual/shiny/app.R
+++ training_and_watering_visual/shiny/app.R
@@ -20,10 +20,15 @@ library(knitr)
 library(kableExtra)
 library(rmarkdown)
 library(formattable)
+library(odbc)
+library(DBI)
+library(RMySQL)
 
 
 source(file.path("functions", "load_mass.R"))
+source(file.path("functions", "load_water.R"))
 source(file.path("functions", "plots_mass.R"))
+
 
 
 
@@ -31,15 +36,28 @@ ui <- fluidPage(
   useShinyjs(),
   navbarPage(
     "Log",
+    position = "fixed-top",
+    # tags$style(type="text/css", "body {padding-top: 70px;}"),
+    
+    
 
     # include the UI for each tab
-    source(file.path("ui", "mass_ui.R"), local = TRUE)$value
+    source(file.path("ui", "mass_ui.R"), local = TRUE)$value,
+    source(file.path("ui", "water_ui.R"), local = TRUE)$value
   )
 )
 
 server <- function(input, output, session) {
   # Include the logic (server) for each tab
   source(file.path("server", "mass_server.R"), local = TRUE)$value
+  source(file.path("server", "water_server.R"), local = TRUE)$value
 }
+
+
+onStop(function() {
+  dbDisconnect(conn = akrami_db)
+})
+
+
 
 shinyApp(ui, server)
