43539ded390883fdfb3df4050237c1427e932e8e
viktorpm <50667179+viktorpm@users.noreply.github.com>
2024-03-15 15:55:46 +0000

    improving documentation and readibility

--- ConvertToRDS.R
+++ ConvertToRDS.R
@@ -1,59 +1,36 @@
+# Function to convert .mat files to .rds format
 ConvertToRDS <- function(file, save_path) {
-  # browser()
+  # Load the R.matlab package for reading .mat files
   library(R.matlab)
-
-  ### tests if save_path was defined
-  if (missing(save_path) == T) {
+  
+  # Check if save_path is provided, if not, set a default path
+  if (missing(save_path)) {
     save_path <- file.path(getwd(), "data", "rds_files")
   }
-
-
-  ### finds the index of the last "/", identifies the start of the file name
-  filename_pos <- gregexpr(file,
-    pattern = "/"
-  ) %>%
+  
+  # Find the position of the last "/" to identify the start of the filename
+  filename_pos <- gregexpr(file, pattern = "/") %>%
     unlist() %>%
     `[[`(length(.)) + 1
-
-  ### saves the filename
-  filename <- substr(
-    file,
-    start = filename_pos,
-    stop = nchar(file)
-  )
-
-
-  ### saves the file path
-  file_path <- substr(
-    file,
-    start = 1,
-    stop = filename_pos - 1
-  )
-
-
-
-  ### gets the list of files from save_path
-  ### !!!!!! Testing outside of the function. Only unprocessed files are passed to the function 
-  # file_list <- list.files(save_path) %>% as.list()
-  # 
-  # filename_test <- paste0(filename, ".rds")
-  # 
-  # if (any(str_detect(string = file_list, pattern = filename_test)) == T) {
-  #   warning(paste0(
-  #     filename,
-  #     " ",
-  #     "File has already been processed"
-  #   ))
-  # } else {
-    rat_data <- readMat(paste0(file_path, filename))
-
-    saveRDS(rat_data,
-      file = paste0(
-        save_path,
-        "/",
-        filename,
-        ".rds"
-      )
-    )
-  # }
+  
+  # Extract the filename from the full path
+  filename <- substr(file, start = filename_pos, stop = nchar(file))
+  
+  # Extract the file path
+  file_path <- substr(file, start = 1, stop = filename_pos - 1)
+  
+  # Read the .mat file using R.matlab's readMat function
+  rat_data <- readMat(paste0(file_path, filename))
+  
+  # Save the data as an .rds file in the specified save_path
+  saveRDS(rat_data, file = paste0(save_path, "/", filename, ".rds"))
 }
+
+# Documentation:
+# Variables:
+# - file: The full path of the .mat file to be converted.
+# - save_path: The path where the converted .rds file will be saved. If not provided, a default path is used.
+# - filename_pos: The position of the last "/" in the file path, used to extract the filename.
+# - filename: The name of the .mat file to be converted.
+# - file_path: The directory path of the .mat file.
+# - rat_data: The data read from the .mat file using R.matlab's readMat function.
--- ExtractSaveData.R
+++ ExtractSaveData.R
@@ -1,113 +1,67 @@
+# Load necessary functions
+source("ConvertToRDS.R") # Function to convert mat files to rds
+source("ReadData.R") # Function to read rds files to a tibble
+source("TRAININGtoCSV.R") # Function to save tibble to csv
+# source("TRAININGtoFeather.R") # Function to save tibble to feather format (commented out)
 
-### loading functions ----
-source("ConvertToRDS.R") ### f. to convert mat files to rds
-source("ReadData.R") ### f. to read rds files to a tibble
-source("TRAININGtoCSV.R") ### f. to save tibble to csv
-# source("TRAININGtoFeather.R") ### f. to save tibble to csv
-
-
-
-### determining the full path of each file ----
-
-### listing all the mat files in the data folders
+# Define the path to the data folder
 in_path <- file.path("/mnt", "ceph","_raw_data", "rat_training_172", "SoloData", "Data")
-file_list <- list.files(file.path("/mnt", "ceph","_raw_data", "rat_training_172", "SoloData", "Data"), 
-                        pattern = "\\.mat$",
-                        recursive = T) %>% 
-  as.list()
 
-### removing test data
-file_list <- file_list[!grepl("experimenter", file_list) & # excluding files from experimenter folder
-                         !grepl("Session Settings", file_list) & 
-                         !grepl("FakeSubject", file_list)]
-# & !grepl("emmett", file_list)] 
-# full_path <- paste0(in_path, "/", file_list) %>% as.list()
+# List all .mat files in the data folder, excluding test data
+file_list <- list.files(in_path, pattern = "\\.mat$", recursive = TRUE)
+file_list <- file_list[!grepl("experimenter|Session Settings|FakeSubject", file_list)]
 
-### listing files already converted to rds 
-rds_list <- list.files(file.path("/mnt", "ceph","_raw_data", "rat_training_172", "rds_files")) %>% as.list()
+# List files already converted to rds
+rds_list <- list.files(file.path("/mnt", "ceph","_raw_data", "rat_training_172", "rds_files"))
 
-### listing the not yet converted mat files (remove ".rds", remove path form character strings)
-### difference between file_list and rds_list
+# Identify not yet converted mat files by comparing base filenames
 not_yet_conv <- setdiff(
   sub(pattern = ".*\\/", "", file_list),  
   rds_list %>% unlist() %>% substr(start = 1, stop = nchar(.)-4)
-) 
-
-
-### adding path to the not yet converted mat files
-ifelse(
-  not_yet_conv %>% is_empty(),
-  to_be_conv <-  character(),
-  to_be_conv <- paste0(
-    in_path, 
-    "/",
-    file_list[str_detect(file_list, pattern = paste(not_yet_conv, collapse = "|")
-    )] %>% unlist()
-  ) 
-  )
-
-
-
-
-### converting not yet converted mat files to rds ----
-ifelse(
-  to_be_conv %>% is_empty(),
-  warning("Nothing to convert"),
-  walk(to_be_conv, ~ ConvertToRDS(file = .x))
 )
 
+# Add path to the not yet converted mat files
+to_be_conv <- ifelse(
+  length(not_yet_conv) == 0,
+  character(),
+  paste0(in_path, "/", file_list[str_detect(file_list, pattern = paste(not_yet_conv, collapse = "|"))])
+)
 
-### reading rds files to a tibble and saving them to a csv file ----
+# Convert not yet converted mat files to rds
+if (length(to_be_conv) > 0) {
+  walk(to_be_conv, ~ ConvertToRDS(file = .x))
+} else {
+  warning("Nothing to convert")
+}
 
-### updated rds_list 
-rds_list <- list.files(file.path("/mnt", "ceph","_raw_data", "rat_training_172", "rds_files")) %>% as.list()
-
+# Read rds files to a tibble and save them to a csv file
+rds_list <- list.files(file.path("/mnt", "ceph","_raw_data", "rat_training_172", "rds_files"))
 to_append <- setdiff(
-  
-  rds_list %>% unlist(),
-    
-  suppressMessages(
-  suppressWarnings(
+  rds_list,
+  suppressMessages(suppressWarnings(
     if (file.exists(file.path("shiny_app", "TRAINING.csv"))) {
-        read_csv(file.path("shiny_app", "TRAINING.csv")) %>% dplyr::select(file) %>% pull()
-      } else
-      {
-        character()
-      }
-      
-      )
-    )
-  ) %>%
-  as.list()
-
+      read_csv(file.path("shiny_app", "TRAINING.csv")) %>% dplyr::select(file) %>% pull()
+    } else {
+      character()
+    }
+  ))
+)
 
 walk(to_append, ~ ReadData(rds_file = .x) %>% TRAININGtoCSV())
-# walk(rds_list, ~ ReadData(rds_file = .x) %>% TRAININGtoFeather())
 
-
-
-################################################
-### Trial by trial data 
-
-walk(rds_list, ~ ReadData(rds_file = .x, trialData = T) %>%
+# Trial by trial data
+walk(rds_list, ~ ReadData(rds_file = .x, trialData = TRUE) %>%
        TRAININGtoCSV(filename = "TrialByTrial.csv"))
 
-
-# data_@AltStatDelayComp_athena_AA08_200217a.mat.rds
-# data_@AthenaDelayComp_viktor_VP12_200213a.mat.rds
-# data_@SoundCategorization_athena_AA05_200217a.mat.rds
-# data_@AltSoundCategorization_sharbat_SC04_200106a.mat.rds
-# LT01_Gap_Detection_20191011_093950.mat.rds
-# 
-# 
-ReadData(rds_file = "LT01_Gap_Detection_20191011_093950.mat.rds",trialData = T) %>%
+# Specific example of trial by trial data
+ReadData(rds_file = "LT01_Gap_Detection_20191011_093950.mat.rds", trialData = TRUE) %>%
   TRAININGtoCSV(filename = "TrialByTrial.csv")
 
-
-
-
-
-
-
- 
-
+# Documentation:
+# Variables:
+# - in_path: The path to the data folder.
+# - file_list: A list of all .mat files in the data folder, excluding test data.
+# - rds_list: A list of files already converted to .rds.
+# - not_yet_conv: A list of .mat files that have not yet been converted to .rds.
+# - to_be_conv: A list of paths to .mat files that need to be converted to .rds.
+# - to_append: A list of .rds files that need to be appended to the TRAINING.csv file.
