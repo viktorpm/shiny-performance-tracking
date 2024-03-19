4f0adfa795da08ec49ac2add8a02f5c4ccd8ab89
Viktor Plattner <plattnerviktor@gmail.com>
2024-03-19 16:05:36 +0000

    creating SoundCatContinuous tab on shiny app

--- shiny_app/app.R
+++ shiny_app/app.R
@@ -33,6 +33,7 @@ source(file.path("functions", "plots_AltSoundCateg.R"))
 source(file.path("functions", "plots_summary.R"))
 source(file.path("functions", "plots_AltSoundCategCatch.R"))
 source(file.path("functions", "plots_SequenceComp.R"))
+source(file.path("functions", "plots_SoundCatContinuous.R"))
 
 # Define the UI for the Shiny app
 ui <- fluidPage(
@@ -46,7 +47,8 @@ ui <- fluidPage(
     source(file.path("ui", "AltSoundCategorization_ui.R"), local = TRUE)$value,
     source(file.path("ui", "AltStat_ui.R"), local = TRUE)$value,
     source(file.path("ui", "AltSoundCategorizationCatch_ui.R"), local = TRUE)$value,
-    source(file.path("ui", "ElenaSequenceComp_ui.R"), local = TRUE)$value
+    source(file.path("ui", "ElenaSequenceComp_ui.R"), local = TRUE)$value,
+    source(file.path("ui", "SoundCatContinuous_ui.R"), local = TRUE)$value
   )
 )
 
@@ -60,6 +62,7 @@ server <- function(input, output, session) {
   source(file.path("server", "AltStat_server.R"), local = TRUE)$value
   source(file.path("server", "AltSoundCategorizationCatch_server.R"), local = TRUE)$value
   source(file.path("server", "ElenaSequenceComp_server.R"), local = TRUE)$value
+  source(file.path("server", "SoundCatContinuous_server.R"), local = TRUE)$value
 }
 
 # Run the Shiny app
